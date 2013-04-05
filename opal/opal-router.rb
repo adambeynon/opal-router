89b3f419313a10ad80b244e8a9fca1a67031de19
Adam Beynon <adam.beynon@gmail.com>
2013-04-05 21:58:44 +0100

    Route matching

--- opal/opal-router.rb
+++ opal/opal-router.rb
@@ -3,6 +3,9 @@ class Router
 
   def initialize
     @routes = []
+    router = self
+
+    `window.addEventListener('hashchange', function() { #{router.update}; }, false)`
   end
 
   def start
@@ -10,7 +13,7 @@ class Router
   end
 
   def route path, &handler
-    @routes << [path, handler]
+    @routes << Route.new(path, &handler)
   end
 
   def update
@@ -18,17 +21,42 @@ class Router
   end
 
   class Route
+
+    # Regexp for matching named params in path
     NAMED = /:(\w+)/
+
+    # Regexp for matching named splats in path
+    SPLAT = /\\\*(\w+)/
 
     attr_reader :regexp, :named
 
-    def initialize pattern
-      @named = []
+    def initialize pattern, &handler
+      @named, @handler = [], handler
+
+      pattern = Regexp.escape pattern
 
       pattern.gsub(NAMED) { |m| @named << m[1..-1] }
+      pattern.gsub(SPLAT) { |m| @named << m[2..-1] }
 
-      pattern = Regexp.escape(pattern).gsub(NAMED, "([^\\/]*)")
+      pattern = pattern.gsub NAMED, "([^\\/]*)"
+      pattern = pattern.gsub SPLAT, "(.*?)"
+
       @regexp = Regexp.new "^#{pattern}$"
+    end
+
+    # Return a hash of all named parts to values if route matches path, or
+    # nil otherwise
+    def match path
+      if match = @regexp.match(path)
+        params = {}
+        @named.each_with_index { |name, i| params[name] = match[i + 1] }
+
+        @handler.call params if @handler
+
+        return true
+      end
+
+      false
     end
   end
 end
--- spec/route_spec.rb
+++ spec/route_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe Router::Route do
   describe "#initialize" do
     let(:route) { Router::Route }
+
     it "creates a regexp from the given pattern" do
       route.new('foo').regexp.should eq(/^foo$/)
     end
@@ -11,7 +12,7 @@ describe Router::Route do
       route.new('/foo/bar/').regexp.should eq(/^\/foo\/bar\/$/)
     end
 
-    it "finds named params in regexp" do
+    it "finds named params in pattern" do
       r = route.new('/foo/:bar')
       r.named.should eq(['bar'])
 
@@ -19,9 +20,45 @@ describe Router::Route do
       p.named.should eq(['woosh', 'kapow'])
     end
 
+    it "finds splatted params in pattern" do
+      route.new('/foo/*baz').named.should eq(['baz'])
+    end
+
     it "produces a regexp to match given pattern" do
       route.new('/foo').regexp.match('/bar').should be_nil
       route.new('/foo').regexp.match('/foo').should be_kind_of(MatchData)
     end
+  end
+
+  describe "#match" do
+    let(:route) { Router::Route }
+
+    it "returns false for a non matching route" do
+      route.new('/foo').match('/a/b/c').should be_false
+    end
+
+    it "returns true for a matching route" do
+      route.new('/foo').match('/foo').should be_true
+    end
+
+    it "calls block given to #initialize on matching a route" do
+      called = false
+      route.new('/foo') { called = true }.match('/foo')
+      called.should be_true
+    end
+
+    it "calls handler with an empty hash for a simple route" do
+      route.new('/foo') { |params| params.should eq({}) }.match('/foo')
+    end
+
+    it "returns a hash of named params for matching route" do
+      route.new('/foo/:first') { |params|
+        params.should eq({'first' => '123' })
+      }.match('/foo/123')
+
+      route.new('/:first/:second') { |params|
+        params.should eq({ 'first' => 'woosh', 'second' => 'kapow' })
+      }.match('/woosh/kapow')
+     end
   end
 end
